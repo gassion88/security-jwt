881c8852a42030a65b9f39c288bc6b1c2acae935
Soslan Gassiev <soslangassity04@gmail.com>
2023-07-23 15:37:59 +0300

    Initial commit

---
+++ src/test/java/com/gassion88/security/jwt/SecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gassion88.security.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
