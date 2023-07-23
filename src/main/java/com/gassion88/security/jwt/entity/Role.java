2c6d5c0e46e33b378a7d480348e4c129b69f4135
Soslan Gassiev <soslangassity04@gmail.com>
2023-07-23 16:12:03 +0300

    feat : add entities

---
+++ src/main/java/com/gassion88/security/jwt/entity/Role.java
@@ -0,0 +1,20 @@
+package com.gassion88.security.jwt.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+}
---
+++ src/main/java/com/gassion88/security/jwt/entity/User.java
@@ -0,0 +1,35 @@
+package com.gassion88.security.jwt.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Collection<Role> roles;
+
+}
